3f14bfc66042c61cf2ab147d6b4d79ad0da5b508
lguzmanperez <94753147+lguzmanperez@users.noreply.github.com>
2022-04-15 20:10:54 -0300

    Agregué NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <header>
+            <h1>Puerto Calzados</h1>
+            <nav>
+                <a href="#">Caballeros </a>
+                <a href="#">Dama </a>
+                <a href="#">Niñx </a>
+            </nav>
+        </header>
+       
+     
+        )
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react"
-import ReactDOM from "react-dom"
-function App(){
+import ReactDOM from "react-dom/client"
+/* import ReactDOM from "react-dom" */
+import App from "./App" 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+/* function App() {
     return "Hola Mundo"
-}
-ReactDOM.render(<App/>, document.getElementById("root"))
+} */
+/* const App = () => {
+    return "Hola Mundo"
+} */
+/* ReactDOM.render(<App/>, document.getElementById("root")) */
+root.render(< App/>)
